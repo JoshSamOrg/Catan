3c57fdc309f7db3def5c0457c6fd06d4c9c663b1
Sam Gearou <samgearou@gmail.com>
2016-07-09 18:27:06 -0700

    Initialization of the project

---
+++ desktop/src/com/catan/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.catan.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.catan.CatanGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CatanGame(), config);
+	}
+}
