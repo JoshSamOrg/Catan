3b833ba44b78eea04aedf633548e4e6749dcbfd4
megabobo <joshgearou@gmail.com>
2016-07-12 08:21:41 -0700

    HexGenerator class

--- desktop/src/com/catan/desktop/DesktopLauncher.java
+++ desktop/src/com/catan/desktop/DesktopLauncher.java
@@ -3,6 +3,7 @@ package com.catan.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.catan.CatanGame;
+import com.catan.HexGenerator;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
