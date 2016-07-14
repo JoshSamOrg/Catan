120ccaef96f7f44275d7dafb5f27245f0ac80afb
megabobo <joshgearou@gmail.com>
2016-07-14 08:49:43 -0700

    random number generator class

---
+++ core/src/com/catan/NumberGenerator.java
@@ -0,0 +1,50 @@
+package com.catan;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class NumberGenerator implements Screen {
+private SpriteBatch batch;
+
+@Override
+public void show() {
+	
+	
+}
+
+@Override
+public void render(float delta) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void resize(int width, int height) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void pause() {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void resume() {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void hide() {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void dispose() {
+	// TODO Auto-generated method stub
+	
+}
+}
